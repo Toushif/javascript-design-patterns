d0ce2e84438200092c27b9aa18191505575599cb
Toushif <toushif.haq@gmail.com>
2021-11-02 04:03:08 +0530

    Null Object pattern added

--- facade-pattern/example-1.js
+++ facade-pattern/example-1.js
@@ -1,4 +1,4 @@
-// The Facade Pattern
+// The Facade Pattern - Structural Patterns
 // When we put up a facade, we present an outward appearance to the world which may conceal a very different reality. This was the inspiration for the name behind the next pattern we're going to review - the Facade pattern. This pattern provides a convenient higher-level interface to a larger body of code, hiding its true underlying complexity. Think of it as simplifying the API being presented to other developers, something which almost always improves usability.
 
 // Facades are a structural pattern which can often be seen in JavaScript libraries like jQuery where, although an implementation may support methods with a wide range of behaviors, only a "facade" or limited abstraction of these methods is presented to the public for use.
